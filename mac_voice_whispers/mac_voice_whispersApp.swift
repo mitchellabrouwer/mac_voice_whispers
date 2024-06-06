cf57dd4beebfdc72466e289104b3f438c9ff6fda
Mitch <mbarron198@gmail.com>
2024-06-06 10:57:10 +1000

    Initial Commit

---
+++ mac_voice_whispers/mac_voice_whispersApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_voice_whispersApp.swift
+//  mac_voice_whispers
+//
+//  Created by Mitch Brouwer on 6/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct mac_voice_whispersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
